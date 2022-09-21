c8e973b5d83c60514021b53d55ae8914d573c8b8
Saif Ali Shaik <saifshine7@gmail.com>
2022-09-21 12:35:36 +0530

    reset

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,36 +14,18 @@ async function renderApp() {
   }
 }
 
-async function renderSidebar() {
-  let viewIssBtn = document.querySelector('.issue-details');
-  let createIssBtn = document.querySelector('.create-issue');
-
-  createIssBtn.addEventListener('fwClick', createIssue)
-  viewIssBtn.addEventListener('fwClick', async function showDetails() {
-
-      await client.interface.trigger('showModal', {
-        title: 'GitHub Issue Details',
-        template: './views/modal.html'
-      });
-
-  })
-}
-
 async function createIssue() {
   var {
     ticket: { id: ticketID, subject, description }
   } = await client.data.get('ticket');
 
-  console.log(ticketID, subject, description);
-
+  console.log(ticketID, subject, description)
   try {
-    let dbKey = String(ticketID);
-    let dbResponse = await client.db.get(dbKey);
-    console.log('dbResponse', dbResponse);
-    await showNotification('warning', `An github issue is already created for ticket number ${dbResponse.ticketID}`);
-   } catch (error) {
-    console.log('error occurs when db key is not found - issue is not alreayd created so creating it. but error', error)
+    let dbKey = String(ticketID).substr(0, 30);
 
+    let dbResponse = await client.db.get(dbKey);
+    await showNotification('warning', `An github issue is already created for ticket number ${dbResponse.ticketID}`);
+  } catch (error) {
     if (error.status && error.message) {
       let { status, message } = error;
       let options = {
@@ -57,41 +39,27 @@ async function createIssue() {
         }),
         isOAuth: true
       };
-<<<<<<< Updated upstream
       let issuesEnpoint = `https://api.github.com/repos/<%= iparam.github_repo %>/issues`;
       let { response } = await client.request.post(issuesEnpoint, options);
 
       console.log('response', response);
 
       let { id: issueID, number: issueNumber } = JSON.parse(response);
-=======
-
-      let issuesEnpoint = `https://api.github.com/repos/<%= iparam.github_repo %>/issues`;
-      let { response } = await client.request.post(issuesEnpoint, options);
-      let { id: issueID, number: issueNumber } = JSON.parse(response);
-
->>>>>>> Stashed changes
       let data = {
         ticketID,
         issueID,
         issueNumber
       };
 
-<<<<<<< Updated upstream
       console.log('data', data);
       await Promise.all([client.db.set(String(issueID), { ...data }), client.db.set(String(ticketID), { ...data })]);
       await showNotification('success', 'Github Issue has been created successfully');
-=======
-      await Promise.all([client.db.set(String(issueID), { ...data }), client.db.set(String(ticketID), { ...data })]);
-      await showNotification('success', 'GitHub Issue has been created successfully');
->>>>>>> Stashed changes
     } else {
       console.error('Here is what we know:', error);
     }
   }
 }
 
-<<<<<<< Updated upstream
 function renderSidebar() {
   const pick = document.querySelector.bind(document);
 
@@ -111,16 +79,11 @@ function renderSidebar() {
   });
 }
 
-=======
->>>>>>> Stashed changes
 async function showNotification(status, message) {
   const details = {
     type: `${status}`,
     message: `${message}`
   };
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
   await client.interface.trigger('showNotify', details);
 }
--- app/scripts/modal.js
+++ app/scripts/modal.js
@@ -1,42 +1,33 @@
-(async function () {
-  let body = document.querySelector('.app-body');
+document.onreadystatechange = async function () {
+  if (document.readyState === 'interactive') {
+    var pick = document.querySelector.bind(document);
+    var body = pick('.app-body');
 
-try {
-    let client = await app.initialized();
+    try {
+      let client = await app.initialized();
 
-    let options = {
+      let options = {
         headers: {
-            Authorization: 'token <%= access_token %>',
-            'user-agent': 'freshworks app'
+          Authorization: 'token <%= access_token %>',
+          'user-agent': 'freshworks app'
         },
         isOAuth: true
-    };
+      };
 
-    let {
-        ticket: {
-            id: ticketID
-        }
-    } = await client.data.get('ticket');
+      let {
+        ticket: { id: ticketID }
+      } = await client.data.get('ticket');
 
-    console.log('Ticket ID', ticketID);
+      console.log('Ticket ID', ticketID);
 
-    let {
-        issueNumber
-    } = await client.db.get(ticketID);
+      let { issueNumber } = await client.db.get(ticketID);
 
-    let issuesEnpoint = `https://api.github.com/repos/<%= iparam.github_repo %>/issues/${issueNumber}`;
+      let issuesEnpoint = `https://api.github.com/repos/<%= iparam.github_repo %>/issues/${issueNumber}`;
 
-    let {
-        response: issueDetails
-    } = await client.request.get(issuesEnpoint, options);
-    let {
-        url,
-        number,
-        title,
-        body: desc
-    } = JSON.parse(issueDetails);
+      let { response: issueDetails } = await client.request.get(issuesEnpoint, options);
+      let { url, number, title, body: desc } = JSON.parse(issueDetails);
 
-    const modalContent = `
+      const modalContent = `
       <h2>${title}</h2>
       <fw-label color="blue" value="Issue Number: ${number}"></fw-label>
       <br>
@@ -44,17 +35,9 @@ try {
       <h3>Description</h3>
       <p>${desc}</p>
       `;
-<<<<<<< Updated upstream
       body.insertAdjacentHTML('afterbegin', modalContent);
     } catch (error) {
       body.insertAdjacentHTML('afterbegin', '<p>No Github issue details associated to this ticket found</p>');
     }
   }
 };
-=======
-    body.insertAdjacentHTML('afterbegin', modalContent);
-} catch (error) {
-    body.insertAdjacentHTML('afterbegin', '<p>No Github issue details associated to this ticket found</p>');
-}
- })();
->>>>>>> Stashed changes
